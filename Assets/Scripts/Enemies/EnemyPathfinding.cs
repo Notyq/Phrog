b246b0f46bc76ffe97d39e41deef493622cee728
jstnde <nahcnitsuj@gmail.com>
2022-08-08 16:39:02 +0800

    update enemy gfx

---
+++ Assets/Scripts/Enemies/EnemyPathfinding.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class EnemyPathfinding : MonoBehaviour
+{
+    public AIPath aiPath;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aiPath.desiredVelocity.x >= .01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (aiPath.desiredVelocity.x <= -.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+}
